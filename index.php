24d3011a51e0e590023c4a77292bb2887d879667
puzol <51291320+puzol@users.noreply.github.com>
2020-09-03 16:59:01 +0200

    za doma

--- index.php
+++ index.php
@@ -56,33 +56,61 @@
                         <?php $length = strlen($row['message']);
                         $isred = $row['isred'];  ?>
                         
-                        <li class="shout <?php return_class($length) ?> <?php redit($isred) ?>"><span><?php echo $row['time'] ?></span> - <?php echo $row['user'] ?>: <?php echo $row['message'] ?></li>
+                        <li class="shout <?php return_class($length) ?> <?php redit($isred) ?>" id="shout"><span><?php echo $row['time'] ?></span> - <?php echo $row['user'] ?>: <?php echo $row['message'] ?></li>
      
                     <?php endwhile; ?> 
-                </ul>            
+                </ul>
+                <p class="writing">{{youAreWriting}}{{ifLonger()}}</p>            
             </div>
 
             <div id="input">
                 <form action="process.php" method="post" class="main_form">
 
                     <div class="input_wrap">
-                    <input type="text" name="user" placeholder="Enter your name" id="text_user">
-                    <input type="text" name="message" placeholder="Enter your message" id="text_message">
+                    <input type="text" name="user" placeholder="Enter your name" id="text_user" v-model="user">
+                    <input type="text" name="message" placeholder="Enter your message" id="text_message" v-model="message" 
+                    oninput="onInputFun(document.getElementById('shouts').scrollTop,document.getElementById('shouts').scrollHeight)" :disabled="user.length < 2">
                     </div>
                     
                     <br>
                     <input type="radio" id="isredid" name="isred" value=0 checked> <label for="isredid" id="blacklabel">I'm zen<img src="zen.svg" alt="angry_svg" title="angry" id="zen"><div id="blacklabel_check"></div></label>
                     <input type="radio" id="isblackid" name="isred" value=1 > <label for="isblackid" id="redlabel">I'm angry<img src="31135.svg" alt="angry_svg" title="angry" id="angry"><div id="redlabel_check"></div></label><br> 
-                    <input type="submit" name="submit" value="Shout it out!" class="shout_btn"> 
+                    <input type="submit" name="submit" value="Shout it out!" class="shout_btn" :disabled="message.length < 1"> 
                 </form>
             </div>
         </div>
 
         <script src="scripts.js"></script>
+        <script src="jqscript.js"></script>
+        
         <script
             src="https://code.jquery.com/jquery-3.5.1.js"
             integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
             crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12"></script>
+
+
+        <script>
+        new Vue({
+        el: '#container',
+        data:{
+            message: "",
+            youAreWriting: "",
+            user:"",
+        },
+        methods: {
+            ifLonger: function(){
+                if(this.message.length > 0){
+                    this.youAreWriting = "Write something polite...or don't";
+                    // container.scrollTop = container.scrollHeight;
+                }else{
+                    this.youAreWriting = "";
+                    container.scrollTop = container.scrollHeight;
+                }
+            }
+        }
+    })
         </script>
         
     </body>
--- scripts.js
+++ scripts.js
@@ -1,9 +1,44 @@
-var shouts_scroll = document.getElementById("shouts");
-shouts_scroll.scrollTop = shouts_scroll.scrollHeight;
+const shouts = document.getElementById("shouts");
+// shouts_scroll.scrollTop = shouts_scroll.scrollHeight;
 
 var textUser = document.getElementById("text_user");
 var textMessage = document.getElementById("text_message");
 
-setTimeout(shouts_scroll.reload(), 500);
+// const getScrollHeight =()=> {
+//     return document.getElementById("shouts").scrollHeight;
+// }
+
+// const setScrollTop =()=>{
+//     let value = 500;
+//     document.getElementById("shouts").scrollTop = value;
+//     console.log(document.getElementById("shouts").scrollTop + "is scroll top");
+// }
+
+// const returnScrollTop =()=>{
+//     return setScrollTop();
+// }
+
+onInputFun = function(scrollTop, scrollHeight){
+    // getScrollHeight();
+    // setScrollTop();
+    setTimeout(function(){
+        document.getElementById("shouts").scrollTop = scrollHeight;
+    },100);
+    console.log("input Detected");
+    // returnScrollTop();
+    console.log(scrollHeight + " is height");
+    console.log(scrollTop + " is top");
+}
+
+const onloadTop =()=>{
+    setTimeout(function(){
+        document.getElementById("shouts").scrollTop = document.getElementById("shouts").scrollHeight;
+    },100);
+}
+
+window.onload = onloadTop();
+
+// setTimeout(shouts_scroll.reload, 500);
+
 
 
